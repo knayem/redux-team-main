d76400b70d3e9c74c44e75e18b981bf4db63e23d
mrakib007 <76766368+mrakib007@users.noreply.github.com>
2021-06-03 23:52:12 +0600

    updated index.js
    add my work

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,1 +1,11 @@
 // Team member will works
+
+import {combineReducers} from 'redux';
+import {teamReducer,selectedTeamReducer} from './teamReducer';
+
+const reducers = combineReducers({
+    allTeams: teamReducer,
+    team: selectedTeamReducer,
+});
+
+export default reducers;
