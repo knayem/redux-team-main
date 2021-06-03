77c9cdc7d6291393d72b1c389f11c75260b6546a
mrakib007 <76766368+mrakib007@users.noreply.github.com>
2021-06-03 23:49:29 +0600

    updated teamActions.js
    Add my work.

--- src/redux/actions/teamActions.js
+++ src/redux/actions/teamActions.js
@@ -1,2 +1,17 @@
 // Team member will works
 
+import {ActionTypes} from "../constants/action-types";
+
+export const setTeams = (teams) => {
+    return {
+        type: ActionTypes.SET_TEAMS,
+        payload: teams,
+    }
+}
+
+export const selectedTeam = (team) => {
+    return {
+        type: ActionTypes.SELECTED_TEAM,
+        payload: team,
+    }
+}
