771dcf1ae2f325a7f0e7a81e9de968abe6a27183
mrakib007 <76766368+mrakib007@users.noreply.github.com>
2021-06-03 23:54:48 +0600

    update store.js
    add my work

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,8 @@
 // Team member will works
+
+import {createStore} from 'redux';
+import reducers from "./reducers/index";
+
+const store = createStore(reducers,{}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+export default store;
